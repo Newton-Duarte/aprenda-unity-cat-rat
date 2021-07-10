5262755e8240f2c6ff0700e02ab6b429de4b32a5
Newton Duarte Cavalcanti <newton_@hotmail.com>
2021-07-10 09:22:39 -0300

    fix: github repository name

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,147 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+internal enum gameState {
+    gameplay, gamewin, gameover
+}
+
+public class GameController : MonoBehaviour
+{
+    int levelTime = 90;
+    int score;
+
+    [Header("Gameplay Config.")]
+    internal gameState currentState;
+    [SerializeField] internal Transform ratHole;
+
+    [Header("Boundary Config.")]
+    [SerializeField] internal Transform leftPlayerBoundary;
+    [SerializeField] internal Transform rightPlayerBoundary;
+
+    [Header("UI Config.")]
+    [SerializeField] Text scoreText;
+    [SerializeField] Text timeText;
+
+    [Header("Audio Config.")]
+    [SerializeField] AudioSource fxMusic;
+    [SerializeField] AudioSource fxSource;
+    [SerializeField] AudioSource fxAlertSource;
+    [SerializeField] AudioClip fxCollect;
+    [SerializeField] AudioClip fxGameWin;
+    [SerializeField] AudioClip fxGameOver;
+    [SerializeField] AudioClip fxEnemy;
+
+    [Header("Enemy Config.")]
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] Transform leftSpawn;
+    [SerializeField] Transform rightSpawn;
+    [SerializeField] int[] enemySpawnTimes;
+    bool isSpawned;
+    internal Transform targetSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeText.text = levelTime.ToString();
+        StartCoroutine(levelCountdown());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isSpawned && currentState == gameState.gameplay)
+        {
+            isSpawned = true;
+            StartCoroutine(spawnEnemy());
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (levelTime == 30)
+        {
+            fxMusic.pitch = 1.3f;
+        }
+    }
+
+    public void setIsSpawned(bool value)
+    {
+        isSpawned = value;
+    }
+
+    IEnumerator spawnEnemy()
+    {
+        yield return new WaitForSeconds(Random.Range(enemySpawnTimes[0], enemySpawnTimes[1]));
+
+        int rand = Random.Range(0, 100);
+
+        if (rand < 50)
+        {
+            fxAlertSource.panStereo = -1;
+            fxAlertSource.PlayOneShot(fxEnemy);
+            yield return new WaitForSeconds(1f);
+
+            GameObject enemy = Instantiate(enemyPrefab);
+            enemy.transform.position = new Vector3(leftSpawn.position.x, enemy.transform.position.y, enemy.transform.position.z);
+            targetSpawn = rightSpawn;
+        }
+        else
+        {
+            fxAlertSource.panStereo = 1;
+            fxAlertSource.PlayOneShot(fxEnemy);
+            yield return new WaitForSeconds(1f);
+
+            GameObject enemy = Instantiate(enemyPrefab);
+            enemy.transform.position = new Vector3(rightSpawn.position.x, enemy.transform.position.y, enemy.transform.position.z);
+            targetSpawn = leftSpawn;
+        }
+    }
+
+    IEnumerator levelCountdown()
+    {
+        yield return new WaitForSeconds(1);
+        levelTime--;
+        timeText.text = levelTime.ToString();
+
+        if (levelTime == 0)
+        {
+            StopCoroutine(levelCountdown());
+            gameOver();
+            yield break;
+        }
+
+        StartCoroutine(levelCountdown());
+    }
+
+    public void gameOver()
+    {
+        Debug.LogError("Game Over!");
+        fxSource.PlayOneShot(fxGameOver);
+        currentState = gameState.gameover;
+        StartCoroutine(loadSceneWithDelay(3, 1.5f));
+    }
+
+    public void setScore(int points)
+    {
+        score += points;
+        scoreText.text = score.ToString();
+        fxSource.PlayOneShot(fxCollect);
+    }
+
+    internal void gameWin()
+    {
+        Debug.Log("Game Win!");
+        fxSource.PlayOneShot(fxGameWin);
+        currentState = gameState.gamewin;
+        StartCoroutine(loadSceneWithDelay(2, 2.5f));
+    }
+
+    internal IEnumerator loadSceneWithDelay(int sceneIndex, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    GameController _gameController;
+    SpriteRenderer playerSr;
+    Rigidbody2D playerRb;
+    Animator playerAnim;
+
+    [SerializeField] float moveSpeed;
+    [SerializeField] float jumpForce;
+    [SerializeField] AudioSource fxSource;
+    [SerializeField] AudioClip fxJump;
+    bool isLookLeft;
+    bool isGrounded;
+    float speedX;
+    float speedY;
+    public LayerMask whatIsGround;
+    public Transform groundCheck;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gameController = FindObjectOfType(typeof(GameController)) as GameController;
+        playerSr = GetComponent<SpriteRenderer>();
+        playerRb = GetComponent<Rigidbody2D>();
+        playerAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_gameController.currentState == gameState.gameplay)
+        {
+            speedX = Input.GetAxisRaw("Horizontal");
+            speedY = playerRb.velocity.y;
+
+            if (!isLookLeft && speedX < 0)
+            {
+                flip();
+            }
+            else if (isLookLeft && speedX > 0)
+            {
+                flip();
+            }
+
+            if (transform.position.x < _gameController.leftPlayerBoundary.position.x)
+            {
+                transform.position = new Vector3(_gameController.leftPlayerBoundary.position.x, transform.position.y, transform.position.z);
+            }
+            else if (transform.position.x > _gameController.rightPlayerBoundary.position.x)
+            {
+                transform.position = new Vector3(_gameController.rightPlayerBoundary.position.x, transform.position.y, transform.position.z);
+            }
+
+            if (Input.GetButtonDown("Jump") && isGrounded)
+            {
+                jump();
+            }
+        }
+        else if (_gameController.currentState == gameState.gamewin)
+        {
+            playerRb.gravityScale = 0;
+            playerRb.velocity = new Vector2(0, 0);
+            isGrounded = true;
+            transform.position = Vector3.MoveTowards(transform.position, _gameController.ratHole.position, 2 * Time.deltaTime);
+
+            if (transform.position == _gameController.ratHole.position)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.02f, whatIsGround);
+        playerRb.velocity = new Vector2(speedX * moveSpeed, speedY);
+    }
+
+    void LateUpdate()
+    {
+        updateAnimations();
+    }
+
+    private void flip()
+    {
+        isLookLeft = !isLookLeft;
+        playerSr.flipX = isLookLeft;
+    }
+
+    void updateAnimations()
+    {
+        playerAnim.SetInteger("speedX", (int)speedX);
+        playerAnim.SetFloat("speedY", speedY);
+        playerAnim.SetBool("isGrounded", isGrounded);
+    }
+
+    void jump()
+    {
+        playerRb.AddForce(new Vector2(playerRb.velocity.x, jumpForce));
+        fxSource.PlayOneShot(fxJump);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch(collision.tag)
+        {
+            case "collectable":
+                Destroy(collision.gameObject);
+                _gameController.setScore(1);
+                break;
+            case "endGame":
+                _gameController.gameWin();
+                break;
+
+            default:
+                break;
+        }
+    }
+}
